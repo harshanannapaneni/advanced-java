3b60630dd7d7ee457447d6ef694748773356ce9a
Harsha Nannapaneni <harshanannapaneni@pop-os.mynetworksettings.com>
2024-06-12 17:12:21 -0400

    Created a new maven project HelloWorldMavenTest

---
+++ HelloWorldMavenTest/src/main/java/Appplication/HelloWorldMavenTest/App.java
@@ -0,0 +1,14 @@
+package Appplication.HelloWorldMavenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(" This is a Maven Project created by harsha for the first time");
+    }
+}
